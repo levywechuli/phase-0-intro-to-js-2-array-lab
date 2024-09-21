6b98989f71527c22f2293ae98ad2d06bf6a1fc8d
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-09-21 05:23:59 +0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push (name);
+    
+}
+function destructivelyPrependCat (name){
+    cats.unshift (name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    return [...cats, name]
+
+}
+function prependCat(name){
+    return [name, ...cats]
+}
+function removeLastCat() {
+    return cats.slice(0, -1); 
+}
+
+const newCatsArray = removeLastCat();
+console.log(newCatsArray); 
+console.log(cats)
+
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
+
+const Cats =removeFirstCat();
+console.log(newCatsArray); 
+console.log(cats); 
